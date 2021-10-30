6442083d4e701d3574c4c2b784b9dd9ee6c5f311
manosbois <92641969+manosbois@users.noreply.github.com>
2021-10-30 17:22:45 +0300

    Update DamageMove.java

--- DamageMove.java
+++ DamageMove.java
@@ -1,16 +1,16 @@
 package Moves;
 public class DamageMove extends Move {
-    private int damage;
+	private int damage;
 	public DamageMove(int energy, String name) {
 		super();
 	}
 	public int getEnergy() {
- 	   return energy;
-    }
-    public String getName() {
- 	   return name;
-    }
+		return energy;
+	}
+	public String getName() {
+		return name;
+	}
 	public int effect() {
- 	   return 9;
-    }
+		return 9;
+	}
 }
