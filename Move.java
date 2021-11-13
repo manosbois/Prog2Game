491cf1c57b1515c3cf63facb80f4ef9d971a8956
EmmanouelaA <92648519+EmmanouelaA@users.noreply.github.com>
2021-11-13 15:05:29 +0200

    Update Move.java

--- Move.java
+++ Move.java
@@ -21,3 +21,28 @@ public class Move {
     	   return String.format("The %s's energy is %d",name,energy);
        }
 }
+
+//NEW
+package Moves;
+public class Move {
+       protected int energy;
+       protected String name;
+       
+       public void Move(int e, String n) {
+    	   this.energy=energy;
+    	   this.name=name;
+       }
+       public int getEnergy() {
+    	   return energy;
+       }
+       public String getName() {
+    	   return name;
+       }
+       public void effect() {
+    	   System.out.println("No move has been selected");
+       }
+       @Override
+      public String toString() {
+    	   return String.format("The %s's energy is %d",name,energy);
+       }
+}
