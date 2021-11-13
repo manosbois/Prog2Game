d22c5bec0e7b5d9846ad3c7b2ed16da072c69c27
EmmanouelaA <92648519+EmmanouelaA@users.noreply.github.com>
2021-11-13 15:03:56 +0200

    Update DamageMove.java

--- DamageMove.java
+++ DamageMove.java
@@ -14,3 +14,17 @@ public class DamageMove extends Move {
 		return 9;
 	}
 }
+
+//NEW
+package Moves;
+public class DamageMove extends Move {
+    private int damage;
+	public DamageMove(int e, String n) {
+		this.energy=energy;
+		this.name=name;
+	}
+	public void effect(Hero mh, Hero g) {
+		int tempHP = (damage * Hero.temp_attack) / (4 * Hero.temp_defense);
+		Hero.tempHP=Hero.tempHP-tempHP;
+	}
+}
