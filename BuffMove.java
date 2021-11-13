e461f3b39ce732c4562cc0282e5253a0a350a2de
EmmanouelaA <92648519+EmmanouelaA@users.noreply.github.com>
2021-11-13 15:04:33 +0200

    Update BuffMove.java

--- BuffMove.java
+++ BuffMove.java
@@ -15,3 +15,18 @@ public class BuffMove extends Move {
 		return 9;
 	}
 }
+
+//NEW
+package Moves;
+public class BuffMove extends Move {
+	public BuffMove(int e, String n) {
+		this.energy=energy;
+		this.name=name;
+	}
+	public void effect(Hero mh, Hero g) {
+		int tempAttack = Hero.tempAttack + Hero.tempAttack * 0.5;
+		int tempArmour = Hero.tempArmour + Hero.tempArmour * 0.5;
+ 	    Hero.setTempAttack(int tempAttack);
+ 	    Hero.setTempArmour(int tempArmour);
+    }
+}
