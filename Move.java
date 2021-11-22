7debd6aa9ff3a78a03154d8e31db311cb754a7bf
AndrGiorgos <93079543+AndrGiorgos@users.noreply.github.com>
2021-11-22 17:31:32 +0200

    Update Move.java

--- Move.java
+++ Move.java
@@ -1,25 +1,27 @@
 public class Move {
-       private int energy;
-       private String name;
-       
-       public Move(int energy , String name) {
-    	   this.energy=energy;
-    	   this.name=name;
-       }
-       public int getEnergy() {
-    	   return energy;
-       }
-       public String getName() {
-    	   return name;
-       }
-       
-       public void effect(Hero hero1 , Hero hero2) {
-    	   System.out.printf("%s used %s.\n\n" , hero1.getName() , this.getName());
-    	   System.out.println("This move had no effect");
-       }
+	private int energy;
+	private String name;
 
-      @Override
-      public String toString() {
-    	   return String.format("%s's energy is %d", name, energy);
-       }
+	public Move(int energy, String name) {
+		this.energy = energy;
+		this.name = name;
+	}
+
+	public int getEnergy() {
+		return energy;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void effect(Hero hero1, Hero hero2) {
+		System.out.printf("%s used %s.\n\n", hero1.getName(), this.getName());
+		System.out.println("This move had no effect");
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s's energy is %d", name, energy);
+	}
 }
