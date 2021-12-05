a965a70b8514b0d599508db56c21778e2959c9d5
manosbois <t8200115@aueb.gr>
2021-12-05 15:29:09 +0200

    Add ProtectiveMove which extends Move

---
+++ ProtectiveMove.java
@@ -0,0 +1,17 @@
+package SuperCharacter.src;
+
+public class ProtectiveMove extends Move {
+
+    public ProtectiveMove(int energy, String name) {
+        super(energy, name);
+    }
+    private static final double MODIFIER = 0.5;
+
+    public double getModifier() { return MODIFIER; }
+
+    public void effect(Character hero1, Character hero2, double modifier) {
+        System.out.printf("%s used %s.%n", hero1.getName(), this.getName());
+        System.out.printf("%s will be partially protected by the shield%n%n",
+                hero1.getName());
+    }
+}
