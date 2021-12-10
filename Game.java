df6107e569634b7dc8d35a6bedb706fc4b361cee
manosbois <92641969+manosbois@users.noreply.github.com>
2021-12-10 17:24:55 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,12 +1,7 @@
 
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-import java.io.IOException;
-
 public class Game {
 
-    public static void main (String [] args) throws UnsupportedAudioFileException,
-            IOException, LineUnavailableException {
+    public static void main (String [] args) {
 
         Stages stage = new Stages();
         stage.stageControl();
