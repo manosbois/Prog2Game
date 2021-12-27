291ce4930275d4f2d93d32c29fc803b98cdd29eb
NikosNikolaidis02 <92633955+NikosNikolaidis02@users.noreply.github.com>
2021-12-27 17:06:19 +0200

    Update BuffMove.java

--- BuffMove.java
+++ BuffMove.java
@@ -18,6 +18,13 @@ public class BuffMove extends Move {
 		hero1.setTempArmour((int) Math.round(tempArmour));
 		System.out.printf("%s's Attack and Armour "
 				+ "were raised by half%n%n", hero1.getName());
+		String message = hero1.getName() + " used " + this.getName() +". " + hero1.getName() +"'s Attack and Armour were raised by half.";
+		Game.graph.modifyMes(Game.graph.mes1, message);
+		try {
+			Thread.sleep(1500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		//Removing Energy from player using the move
 		hero1.setTempEnergy(hero1.getTempEnergy() - this.getEnergy());
 	}
