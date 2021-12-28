7eb6b403632d4baf6bf33c203b90c81026b91a7d
manosbois <92641969+manosbois@users.noreply.github.com>
2021-12-28 04:06:11 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,14 +1,6 @@
-
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-import java.io.IOException;
-
 public class Game {
-
-    public static void main (String [] args) throws UnsupportedAudioFileException,
-            IOException, LineUnavailableException {
-
-        Stages stage = new Stages();
-        stage.stageControl();
+    static Graph graph;
+    public static void main(String [] args){
+        graph = new Graph("CODERUNNERS");
     }
 }
