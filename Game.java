5e343842f2ac868c7d9ca9698d9041b457bc4d96
manosbois <92641969+manosbois@users.noreply.github.com>
2022-01-09 17:48:06 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,5 +1,21 @@
+/**
+ * Our game's program main class.
+ *
+ * @depend - - - gr.aueb.dmst.gameName.Graph
+ * @depend - - - gr.aueb.dmst.gameName.Battle
+ * @depend - - - gr.aueb.dmst.gameName.Stages
+ * @depend - - - gr.aueb.dmst.gameName.Move
+ * @depend - - - gr.aueb.dmst.gameName.BuffMove
+ * @depend - - - gr.aueb.dmst.gameName.DamageMove
+ * @depend - - - gr.aueb.dmst.gameName.ProtectiveMove
+ * @depend - - - gr.aueb.dmst.gameName.Character
+ * @depend - - - gr.aueb.dmst.gameName.God
+ * @depend - - - gr.aueb.dmst.gameName.Hero
+ */
 public class Game {
+    /** A Graph instance accessible to other classes */
     static Graph graph;
+
     public static void main(String [] args){
         graph = new Graph("CODERUNNERS");
     }
