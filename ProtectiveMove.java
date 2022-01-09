f6cd01fbe2263dcf2755b42ea9ab2a33aa05054b
manosbois <92641969+manosbois@users.noreply.github.com>
2022-01-09 17:49:50 +0200

    Update ProtectiveMove.java

--- ProtectiveMove.java
+++ ProtectiveMove.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 
 public class ProtectiveMove extends Move {
 
-    public ProtectiveMove(int energy, String name, String messageFileName, URL sound) {
+    public ProtectiveMove(int energy, String name,
+                          String messageFileName, URL sound) {
         super(energy, name, messageFileName, sound);
     }
     private static final double MODIFIER = 0.5;
@@ -18,16 +19,18 @@ public class ProtectiveMove extends Move {
     public void effect(Character hero1, Character hero2, double modifier) {
 
         Scanner myReader = new Scanner(new LineNumberReader(
-                new InputStreamReader(Objects.requireNonNull
-                        (this.getClass().getResourceAsStream(
+                new InputStreamReader(Objects.requireNonNull(
+                        this.getClass().getResourceAsStream(
                                 this.getMessageFileName())), StandardCharsets.UTF_8)));
 
         System.out.printf("%s used %s.%n", hero1.getName(), this.getName());
         System.out.printf("%s will be partially protected by the shield%n%n",
                 hero1.getName());
+        Game.graph.moveImage(hero1);
         makeSound(getSound());
         Game.graph.modifyMes(myReader.nextLine() + hero1.getName()
-                + myReader.nextLine() + this.getName() + ". " + myReader.nextLine() + hero1.getName() + myReader.nextLine());
+                + myReader.nextLine() + this.getName() + ". " + myReader.nextLine()
+                + hero1.getName() + myReader.nextLine());
         try {
             Thread.sleep(WAIT_TIME);
         } catch (InterruptedException e) {
