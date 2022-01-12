dffd516b58581c13bc9e82485e1f1baaa5d8db09
AndrGiorgos <93079543+AndrGiorgos@users.noreply.github.com>
2022-01-12 03:37:33 +0200

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -1,6 +1,7 @@
 
 public class Hero extends Character {
 
+	/** Class Constructor */
 	public Hero(int tempHP, int tempAttack, int tempArmour,
 				int tempEnergy) {
 
@@ -10,36 +11,48 @@ public class Hero extends Character {
 		this.energy = tempEnergy;
 	}
 
-	// Stats
+	
+	/** The attack of the Hero */
 	private int attack;
+	/** The armour of the Hero */
 	private int armour;
+	/** The energy of the Hero */
 	private int energy;
 
 
-	// Getters & Setters for Stats
+	
+	/** @return the Attack of the Hero */
 	public int getAttack() {
 		return attack;
 	}
 
+	/** @param attack, set the attack of the Hero */
 	public void setAttack(int attack) {
 		this.attack = attack;
 	}
 
+	/** @return the Armour of the Hero */
 	public int getArmour() {
 		return armour;
 	}
 
+	/** @param armour, set the armour of the Hero */
 	public void setArmour(int armour) {
 		this.armour = armour;
 	}
 
+	/** @return the Energy of the Hero */
 	public int getEnergy() {
 		return energy;
 	}
 
-	public void setEnergy(int energy) { this.energy = energy; }
+	/** @param energy, set the energy of the Hero */
+	public void setEnergy(int energy) { 
+		this.energy = energy; 
+	}
 
-	// Useful methods
+	
+	/** Method that sets the Statistics of the Hero */
 	public void setStats(int hp, int attack, int armour, int energy) {
 		super.setHp(hp);
 		setAttack(attack);
