3c86f2a8ebe82edd5f708b8136a3be6d285aed8d
manosbois <92641969+manosbois@users.noreply.github.com>
2022-01-14 00:56:29 +0200

    Update God.java

--- God.java
+++ God.java
@@ -1,73 +1,92 @@
+package gr.aueb.dmst.gameName;
 
-
+/** Class to help implement the stats of the Gods 
+ * Gods have only Temp stats and HP.
+ */
 public class God extends Character {
 
+    /** Class Constructor 
+     * The Gods are created depending on the numOfBattle. 
+     *@param numOfBattle, the number of the Battle.
+     */
     public God(int numOfBattle) {
+
         super(); //This line is needed but not useful
         switch (numOfBattle) {
-            // Hestia
+            // Dionysus
             case 1:
                 this.setName("Dionysus");
-                setTempStats(100, 100, 100, 10);
+                setTempStats(1.0* Stages.myHero.getHp(),1.0* Stages.myHero.getAttack(),1.0* Stages.myHero.getArmour(), 8);
                 break;
             // Hephaestus
             case 2:
                 this.setName("Hephaestus");
-                setTempStats(110, 110, 110, 10);
+                setTempStats(1.05* Stages.myHero.getHp(),1.05* Stages.myHero.getAttack(),1.05* Stages.myHero.getArmour(), 8);
                 break;
             // Hermes
             case 3:
                 this.setName("Hermes");
-                setTempStats(120, 120, 120, 10);
+                setTempStats(1.075* Stages.myHero.getHp(),1.075* Stages.myHero.getAttack(),1.075* Stages.myHero.getArmour(), 8);
                 break;
             // Demetra
             case 4:
                 this.setName("Demetra");
-                setTempStats(130, 130, 130, 10);
+                setTempStats(1.1* Stages.myHero.getHp(),1.1* Stages.myHero.getAttack(),1.1* Stages.myHero.getArmour(), 8);
                 break;
             // Aphrodite
             case 5:
                 this.setName("Aphrodite");
-                setTempStats(140, 140, 140, 10);
+                setTempStats(1.125* Stages.myHero.getHp(),1.125* Stages.myHero.getAttack(),1.125* Stages.myHero.getArmour(), 8);
                 break;
             // Apollo
             case 6:
                 this.setName("Apollo");
-                setTempStats(150, 150, 150, 10);
+                setTempStats(1.125* Stages.myHero.getHp(),1.125* Stages.myHero.getAttack(),1.125* Stages.myHero.getArmour(), 8);
                 break;
             // Artemis
             case 7:
                 this.setName("Artemis");
-                setTempStats(160, 160, 160, 10);
-                break;
-            // Poseidon
-            case 8:
-                this.setName("Poseidon");
-                setTempStats(170, 170, 170, 10);
+                setTempStats(1.15* Stages.myHero.getHp(),1.15* Stages.myHero.getAttack(),1.15* Stages.myHero.getArmour(), 10);
                 break;
             // Ares
-            case 9:
+            case 8:
                 this.setName("Ares");
-                setTempStats(180, 180, 180, 10);
+                setTempStats(1.15* Stages.myHero.getHp(),1.15* Stages.myHero.getAttack(),1.15* Stages.myHero.getArmour(), 10);
                 break;
             // Hera
-            case 10:
+            case 9:
                 this.setName("Hera");
-                setTempStats(190, 190, 190, 10);
+                setTempStats(1.175* Stages.myHero.getHp(),1.175* Stages.myHero.getAttack(),1.175* Stages.myHero.getArmour(), 10);
+                break;
+            // Poseidon
+            case 10:
+                this.setName("Poseidon");
+                setTempStats(1.175* Stages.myHero.getHp(),1.175* Stages.myHero.getAttack(),1.175* Stages.myHero.getArmour(), 11);
                 break;
             // Athena
             case 11:
                 this.setName("Athena");
-                setTempStats(200, 200, 200, 10);
+                setTempStats(1.225* Stages.myHero.getHp(),1.225* Stages.myHero.getAttack(),1.225* Stages.myHero.getArmour(), 13);
                 break;
             // Zeus
             case 12:
                 this.setName("Zeus");
-                setTempStats(210, 210, 210, 10);
+                setTempStats(1.33* Stages.myHero.getHp(),1.33* Stages.myHero.getAttack(),1.33* Stages.myHero.getArmour(), 33);
                 break;
             default:
                 System.out.println("There is no such god");
         }
+    }
+
+    /** Method that sets the Statistics of the God 
+     * and turns doubles into integers for better gaming experience. 
+     */
+    public void setTempStats(double hp, double attack, double armour, int energy) {
+        setTempHP((int) hp);
+        setTempAttack((int) attack);
+        setTempArmour((int) armour);
+        setTempEnergy(energy);
+        setHp((int) hp);
     }
 
 }
