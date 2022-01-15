01000ce3083aee30693913a71cb584aed37e8cee
manosbois <92641969+manosbois@users.noreply.github.com>
2022-01-15 18:18:20 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,24 +1,24 @@
-package gr.aueb.dmst.gameName;
+package gr.aueb.dmst.GodsNemesis;
 
 /**
  * Our game's program main class.
  *
- * @depend - - - gr.aueb.dmst.gameName.Graph
- * @depend - - - gr.aueb.dmst.gameName.Battle
- * @depend - - - gr.aueb.dmst.gameName.Stages
- * @depend - - - gr.aueb.dmst.gameName.Move
- * @depend - - - gr.aueb.dmst.gameName.BuffMove
- * @depend - - - gr.aueb.dmst.gameName.DamageMove
- * @depend - - - gr.aueb.dmst.gameName.ProtectiveMove
- * @depend - - - gr.aueb.dmst.gameName.Character
- * @depend - - - gr.aueb.dmst.gameName.God
- * @depend - - - gr.aueb.dmst.gameName.Hero
+ * @depend - - - gr.aueb.dmst.GodsNemesis.Graph
+ * @depend - - - gr.aueb.dmst.GodsNemesis.Battle
+ * @depend - - - gr.aueb.dmst.GodsNemesis.Stages
+ * @depend - - - gr.aueb.dmst.GodsNemesis.Move
+ * @depend - - - gr.aueb.dmst.GodsNemesis.BuffMove
+ * @depend - - - gr.aueb.dmst.GodsNemesis.DamageMove
+ * @depend - - - gr.aueb.dmst.GodsNemesis.ProtectiveMove
+ * @depend - - - gr.aueb.dmst.GodsNemesis.Character
+ * @depend - - - gr.aueb.dmst.GodsNemesis.God
+ * @depend - - - gr.aueb.dmst.GodsNemesis.Hero
  */
 public class Game {
     /** A Graph instance accessible to other classes */
     static Graph graph;
 
     public static void main(String [] args){
-        graph = new Graph("CODERUNNERS");
+        graph = new Graph("Gods' Nemesis");
     }
 }
